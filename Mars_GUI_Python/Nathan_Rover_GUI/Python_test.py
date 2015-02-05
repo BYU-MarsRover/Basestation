ecde30ad8374c116f2d10038c116535b09576018
warnern <nathan.arts.warner@gmail.com>
2015-02-04 19:09:59 -0700

    creating a test python to commit to github

---
+++ Mars_GUI_Python/Nathan_Rover_GUI/Python_test.py
@@ -0,0 +1,17 @@
+#######################
+# Author: Nathan Warner
+# created: 2 - 4 - 15
+#######################
+
+## convetions to be determined
+
+import socket
+import signal
+import sys
+import string
+
+def main:
+
+
+class TestClass:
+
