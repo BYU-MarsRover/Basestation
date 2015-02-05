ae6babdcfe2c0fc5798a98df4f92622c568112ac
warnern <nathan.arts.warner@gmail.com>
2015-02-04 19:29:26 -0700

    saving my practice and notes

--- Mars_GUI_Python/Nathan_Rover_GUI/Python_test.py
+++ Mars_GUI_Python/Nathan_Rover_GUI/Python_test.py
@@ -10,8 +10,22 @@ import signal
 import sys
 import string
 
-def main:
+def main():
 
-
-class TestClass:
-
+# A class is defined by using the class keyword followed by the class name and a colon
+# usage: class <className>:
+class Person:
+	# class constructors are created/defined as follows
+	#			<Note arguments>
+	def __init__(name, age, gender):
+		self.Name = name
+		self.Age = age
+		self.Gender = gender
+		
+	def ageAYear():
+		self.Age += 1
+	
+	def setGender(newgender):
+		self.Gender = newgender
+	
+	
