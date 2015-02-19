e3ae2aeb0ac9e523db8d8145a23d4f0564a1f8e7
Joseph DeVictoria <jldevictoria@gmail.com>
2015-02-18 18:19:23 -0700

    Uploaded a new simple Java server and client.

---
+++ Mars_Gui_Java_II/src/mars/Client.java
@@ -0,0 +1,73 @@
+// Filename:        Client.java
+// Creator:         Joseph DeVictoria
+// Creation Date:   February 18, 2015
+// Purpose:         Attempt at implementing a client emulator for our
+//                  Mars rover base station.
+
+package mars;
+
+import java.net.*;
+import java.util.*;
+
+public class Client{
+
+    private final static int PACKETSIZE = 100 ;
+    private DatagramSocket socket;
+    private InetAddress host;
+    private DatagramPacket packet;
+    private int port;
+    private Scanner input;
+
+    public Client(String hostIn, String portIn){
+        try{
+            // Convert the arguments first, to ensure that they are valid
+            host = InetAddress.getByName(hostIn);
+            port = Integer.parseInt(portIn);
+            
+            // Construct the socket
+            socket = new DatagramSocket() ;
+
+            // Create input Scanner.
+            input = new Scanner(System.in);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void run(){
+        try{
+            while(true){
+                // Construct the datagram packet
+                System.out.println("Please give me a string to send:");
+                String tempData = input.nextLine();
+                System.out.println("Sending: " + tempData);
+                byte [] data = tempData.getBytes() ;
+                packet = new DatagramPacket(data,data.length,host,port);
+
+                // Send it
+                socket.send(packet);
+
+                // Set a receive timeout, 2000 milliseconds
+                socket.setSoTimeout(2000);
+
+                // Prepare the packet for receive
+                packet.setData(new byte[PACKETSIZE]);
+
+                // Wait for a response from the server
+                socket.receive(packet);
+
+                // Print the response
+                System.out.println("Received: " + new String(packet.getData()));
+            }
+        }
+        catch( Exception e ){
+            System.out.println(e);
+        }
+        finally{
+            if(socket != null)
+                socket.close() ;
+        }
+    }
+}
+
---
+++ Mars_Gui_Java_II/src/mars/Mars.java
@@ -0,0 +1,36 @@
+// Filename:        Mars.java
+// Creator:         Joseph DeVictoria
+// Creation Date:   February 18, 2015
+// Purpose:         Driver for Server/Client-emulator of Mars Rover Basestation.
+
+package mars;
+
+public class Mars{
+    
+    public static void main(String args[]){
+        // Check arguments for matching usage cases.
+        /*if ((args.length != 3) || (args[0] != "-S")){
+            if (args[0] != "-C"){
+                printUsage();
+                return;
+            }
+        }*/
+
+        if (args[0].equals("-S")){
+            Server marsServer =  new Server(args[1], args[2]);
+            marsServer.run();
+        }
+
+        if (args[0].equals("-C")){
+            Client marsClient = new Client(args[1], args[2]);
+            marsClient.run();
+        }
+    }
+
+    public static void printUsage(){
+        System.out.println("USAGE:");
+        System.out.println("SERVER: java Mars -S host port"); 
+        System.out.println("CLIENT: java Mars -C host port");
+    }
+
+}
